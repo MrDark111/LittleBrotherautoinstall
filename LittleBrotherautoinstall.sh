2f93e1d568f0edd3a9dd9eacf802b42f6dbf41b2
MrDark111 <73544166+MrDark111@users.noreply.github.com>
2021-06-20 14:02:28 +0530

    Add files via upload

---
+++ LittleBrotherautoinstall.sh
@@ -0,0 +1,60 @@
+echo
+
+cd
+
+clear
+
+apt update 
+
+apt upgrade
+
+termux-setup-storage
+
+clear
+
+cd
+
+pkg install python python2 git colorama 
+
+clear 
+
+rm -rf LittleBrother
+
+rm -rf Littlebrother 
+
+rm -rf littleBrother 
+
+rm -rf littleBrother
+
+clear
+
+cd 
+
+
+git clone https://github.com/lulz3xploit/LittleBrother
+
+cd LittleBrother
+
+chmod 777 LittleBrother.py
+
+clear
+
+python LittleBrother.py  
+
+
+
+clear
+
+
+
+cd
+
+
+
+cd 
+
+clear
+
+rm -rf  LittleBrotherautoinstall
+
+echo   code by mr dark
